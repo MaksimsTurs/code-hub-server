b6555c370fffc173972ece18b077e8970443be6f
Maksims Turs <maksims07turs@gmail.com>
2025-07-20 20:41:53 +0200

    v0.0.5

--- src/configuration/start-listening-server.config.ts
+++ src/configuration/start-listening-server.config.ts
@@ -1,6 +1,6 @@
 import { logger } from "../index.js";
 
-import CONFIG_CONST from "@root/CONFIG.const.js";
+import CONFIG_CONST from "../CONFIG.const.js";
 
 export default function startListeningServer(): void {
 	logger.in("dev", "prod").info(CONFIG_CONST.IS_DEV ? `Start Listening port ${CONFIG_CONST.PORT}.` : "Start Listening server.");
